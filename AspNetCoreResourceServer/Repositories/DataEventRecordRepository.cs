25cba11199c449b163bbef5dda49c0451f20ec93
damienbod <damien_bod@hotmail.com>
2017-04-09 17:39:13 +0200

    updating projects

--- AspNetCoreResourceServer/Repositories/DataEventRecordRepository.cs
+++ AspNetCoreResourceServer/Repositories/DataEventRecordRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using AspNet5SQLite.Model;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,13 +10,11 @@ namespace AspNet5SQLite.Repositories
     {
         private readonly DataEventRecordContext _context;
         private readonly ILogger _logger;
-        private IDataProtector _protector;
 
-        public DataEventRecordRepository(DataEventRecordContext context, ILoggerFactory loggerFactory, IDataProtectionProvider provider)
+        public DataEventRecordRepository(DataEventRecordContext context, ILoggerFactory loggerFactory)
         {
             _context = context;
             _logger = loggerFactory.CreateLogger("IDataEventRecordResporitory");
-            _protector = provider.CreateProtector("DataEventRecordRepository.v1");
         }
 
         public List<DataEventRecord> GetAll()
@@ -29,21 +26,18 @@ namespace AspNet5SQLite.Repositories
         public DataEventRecord Get(long id)
         {
             var dataEventRecord = _context.DataEventRecords.First(t => t.Id == id);
-            unprotectDescription(dataEventRecord);
             return dataEventRecord;
         }
 
         [HttpPost]
         public void Post(DataEventRecord dataEventRecord )
         {
-            protectDescription(dataEventRecord);
             _context.DataEventRecords.Add(dataEventRecord);
             _context.SaveChanges();
         }
 
         public void Put(long id, [FromBody]DataEventRecord dataEventRecord)
         {
-            protectDescription(dataEventRecord);
             _context.DataEventRecords.Update(dataEventRecord);
             _context.SaveChanges();
         }
@@ -53,20 +47,6 @@ namespace AspNet5SQLite.Repositories
             var entity = _context.DataEventRecords.First(t => t.Id == id);
             _context.DataEventRecords.Remove(entity);
             _context.SaveChanges();
-        }
-
-        private void protectDescription(DataEventRecord dataEventRecord)
-        {
-            // TODO add this when keys work again
-            var protectedData = _protector.Protect(dataEventRecord.Description);
-            dataEventRecord.Description = protectedData;
-        }
-
-        private void unprotectDescription(DataEventRecord dataEventRecord)
-        {
-            // TODO add this when keys work again
-            var unprotectedData = _protector.Unprotect(dataEventRecord.Description);
-            dataEventRecord.Description = unprotectedData;
         }
     }
 }
