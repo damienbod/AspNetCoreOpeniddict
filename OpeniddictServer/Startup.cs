e58f7979b8b6ddb106ceb93ab6a28fd83f696de6
damienbod <damien_bod@hotmail.com>
2017-04-11 15:11:04 +0200

    code clean up

--- OpeniddictServer/Startup.cs
+++ OpeniddictServer/Startup.cs
@@ -47,21 +47,13 @@ namespace OpeniddictServer
         {
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                // Configure the context to use an in-memory store.
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-
-                // Register the entity sets needed by OpenIddict.
-                // Note: use the generic overload if you need
-                // to replace the default OpenIddict entities.
                 options.UseOpenIddict();
             });
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            // Configure Identity to use the same JWT claims as OpenIddict instead
-            // of the legacy WS-Federation claims it uses by default (ClaimTypes),
-            // which saves you from doing the mapping in your authorization controller.
             services.Configure<IdentityOptions>(options =>
             {
                 options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
@@ -69,10 +61,8 @@ namespace OpeniddictServer
                 options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
             });
 
-            // Register the OpenIddict services.
             services.AddOpenIddict(options =>
             {
-                // Register the Entity Framework stores.
                 options.AddEntityFrameworkCoreStores<ApplicationDbContext>();
 
                 // Register the ASP.NET Core MVC binder used by OpenIddict.
@@ -80,42 +70,15 @@ namespace OpeniddictServer
                 // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
                 options.AddMvcBinders();
 
-                // Enable the authorization, logout, userinfo, and introspection endpoints.
                 options.EnableAuthorizationEndpoint("/connect/authorize")
                        .EnableLogoutEndpoint("/connect/logout")
                        .EnableIntrospectionEndpoint("/connect/introspect")
                        .EnableUserinfoEndpoint("/api/userinfo");
 
-                // Note: the sample only uses the implicit code flow but you can enable
-                // the other flows if you need to support implicit, password or client credentials.
                 options.AllowImplicitFlow();
 
-                // During development, you can disable the HTTPS requirement.
-                options.DisableHttpsRequirement();
-
-                // Register a new ephemeral key, that is discarded when the application
-                // shuts down. Tokens signed using this key are automatically invalidated.
-                // This method should only be used during development.
-                //options.AddEphemeralSigningKey();
-
-                // On production, using a X.509 certificate stored in the machine store is recommended.
-                // You can generate a self-signed certificate using Pluralsight's self-cert utility:
-                // https://s3.amazonaws.com/pluralsight-free/keith-brown/samples/SelfCert.zip
                 options.AddSigningCertificate(_cert);
 
-                // options.AddSigningCertificate("7D2A741FE34CC2C7369237A5F2078988E17A6A75");
-                //
-                // Alternatively, you can also store the certificate as an embedded .pfx resource
-                // directly in this assembly or in a file published alongside this project:
-                //
-                //options.AddSigningCertificate(
-                //    assembly: typeof(Startup).GetTypeInfo().Assembly,
-                //    resource: "damienbodserver.pfx",
-                //    password: "");
-
-                // Note: to use JWT access tokens instead of the default
-                // encrypted format, the following line is required:
-                //
                 options.UseJsonWebTokens();
             });
 
@@ -174,33 +137,6 @@ namespace OpeniddictServer
             app.UseJwtBearerAuthentication(jwtOptions);
 
             app.UseIdentity();
-
-            ////app.UseWhen(context => !context.Request.Path.StartsWithSegments("/api"), branch =>
-            ////{
-                
-
-                
-            ////});
-
-            ////app.UseWhen(context => context.Request.Path.StartsWithSegments("/api"), branch =>
-            ////{
-            ////    //var jwtOptions = new JwtBearerOptions()
-            ////    //{
-            ////    //    AutomaticAuthenticate = true,
-            ////    //    AutomaticChallenge = true,
-            ////    //    RequireHttpsMetadata = false,
-            ////    //    Audience = "https://localhost:44319",
-            ////    //    ClaimsIssuer = "https://localhost:44319/"
-            ////    //};
-
-            ////    //jwtOptions.TokenValidationParameters.ValidAudience = "https://localhost:44319";
-            ////    //jwtOptions.TokenValidationParameters.ValidIssuer = "https://localhost:44319/";
-            ////    //jwtOptions.TokenValidationParameters.IssuerSigningKey = new RsaSecurityKey(_cert.GetRSAPrivateKey().ExportParameters(false));
-            ////    //branch.UseJwtBearerAuthentication(jwtOptions);
-            ////    //branch.UseOAuthValidation();
-            ////});
-
-
 
             app.UseOpenIddict();
 
