c30c57853855b94665ab3ea5287087c927214400
Damien <damien_bod@hotmail.com>
2020-12-26 17:51:28 +0100

    Working test

--- ResourceServer/Controllers/DataEventRecordController.cs
+++ ResourceServer/Controllers/DataEventRecordController.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ResourceServer.Controllers
 {
-    [Authorize("dataEventRecordsPolicy")]
+    //[Authorize("dataEventRecordsPolicy")]
+    [Authorize]
     [Route("api/[controller]")]
     public class DataEventRecordsController : Controller
     {
