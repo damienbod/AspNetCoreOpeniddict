2473a77a40d53e1503dff3e1968c3a3c96015156
damienbod <damien_bod@hotmail.com>
2022-01-04 22:38:52 +0100

    fix CORS for SPA

--- OpeniddictServer/Startup.cs
+++ OpeniddictServer/Startup.cs
@@ -163,6 +163,9 @@ public class Startup
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             //app.UseHsts();
         }
+
+        app.UseCors("AllowAllOrigins");
+
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
