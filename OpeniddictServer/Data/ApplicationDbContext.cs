79a09cdc303cf02e6ca83b2b8fc5da3bca5bb824
damienbod <damien_bod@hotmail.com>
2022-07-01 10:37:36 +0200

    Add ASP.NET Core FIDO2 support

--- OpeniddictServer/Data/ApplicationDbContext.cs
+++ OpeniddictServer/Data/ApplicationDbContext.cs
@@ -1,10 +1,20 @@
+﻿using Fido2Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace OpeniddictServer.Data;
 
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
+
+    public DbSet<FidoStoredCredential> FidoStoredCredential => Set<FidoStoredCredential>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<FidoStoredCredential>().HasKey(m => m.Id);
+
+        base.OnModelCreating(builder);
+    }
 }
---
+++ OpeniddictServer/Fido2/Fifo2UserTwoFactorTokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Fido2Identity;
+
+public class Fifo2UserTwoFactorTokenProvider : IUserTwoFactorTokenProvider<IdentityUser>
+{
+    public Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<IdentityUser> manager, IdentityUser user)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task<string> GenerateAsync(string purpose, UserManager<IdentityUser> manager, IdentityUser user)
+    {
+        return Task.FromResult("fido2");
+    }
+
+    public Task<bool> ValidateAsync(string purpose, string token, UserManager<IdentityUser> manager, IdentityUser user)
+    {
+        return Task.FromResult(true);
+    }
+}
