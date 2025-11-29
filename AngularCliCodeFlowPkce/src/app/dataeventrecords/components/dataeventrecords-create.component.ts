690b31c538687d39a190dcf3aeccd49d682e4b65
damienbod <damien_bod@hotmail.com>
2025-11-29 19:16:25 +0100

    test

--- AngularCliCodeFlowPkce/src/app/dataeventrecords/components/dataeventrecords-create.component.ts
+++ AngularCliCodeFlowPkce/src/app/dataeventrecords/components/dataeventrecords-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { OidcSecurityService, ConfigAuthenticatedResult, AuthenticatedResult } from 'angular-auth-oidc-client';
 
